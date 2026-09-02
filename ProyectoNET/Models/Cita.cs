a00605c4b61d2381848c5a4db446a0f2dae67439
martinalejandrovidaldelhierro <martinalejandrovidaldelhierro@gmail.com>
2026-09-02 00:16:32 -0500

    Add files via upload

---
+++ ProyectoNET/Models/Cita.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaConsultorioSOAP.Models
+{
+    public class Cita
+    {
+        public int IdCita { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Hora { get; set; }
+        public string Motivo { get; set; }
+        public string Tratamiento { get; set; }
+        public bool Estado { get; set; }
+        public int IdPaciente { get; set; }
+    }
+}
---
+++ ProyectoNET/Models/Paciente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaConsultorioSOAP.Models
+{
+    public class Paciente
+    {
+        public int IdPaciente { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public bool Estado { get; set; }
+    }
+}
